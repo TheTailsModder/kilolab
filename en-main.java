ebc218f078262ccdb630bf44b9e2c413f9d9b3c6
ftvTails <85840601+ftvTails@users.noreply.github.com>
2021-06-30 15:49:26 +0400

    Add files via upload

---
+++ en-main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner miles_input = new Scanner(System.in);
+		double miles;
+		double kilometers;
+		System.out.println("\n\t\tKilolab\n");
+		System.out.print("Type miles: ");
+		miles = miles_input.nextDouble();
+		System.out.println("Convertating...");
+		kilometers = miles * 1.609;
+		System.out.println("Kilometers: " + kilometers + " [approximate]");
+		System.out.println("\nThanks for using!\nFollow my GitHub page: ftvTails");
+	}
+}
---
+++ ru-main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner miles_input = new Scanner(System.in);
+		double miles;
+		double kilometers;
+		System.out.println("\n\t\tKilolab\n");
+		System.out.print("Введите мили: ");
+		miles = miles_input.nextDouble();
+		System.out.println("Конвертация...");
+		kilometers = miles * 1.609;
+		System.out.println("Километров: " + kilometers + " [приблизительно]");
+		System.out.println("\nСпасибо за использование!\nСледи за моей страницей на GitHub: ftvTails");
+	}
+}
